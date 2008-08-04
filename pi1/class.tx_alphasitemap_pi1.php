6f4b2223f8e711e2d7ce00da33f70ac52da13593
steffenk <steffenk@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-04 18:09:51 +0000

    compat-version fixed additional settings for menu SQL: includeNotInMenu, addWhere
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/alpha_sitemap/trunk@10942 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_alphasitemap_pi1.php
+++ pi1/class.tx_alphasitemap_pi1.php
@@ -53,69 +53,84 @@ class tx_alphasitemap_pi1 extends tslib_pibase {
 	function main($content, $conf)	{
 		$this->pi_setPiVarDefaults();
 		$this->pi_loadLL();
-        
-        	// Get the PID from which to make the menu.
+		
+			// Get the PID from which to make the menu.
 			// If a page is set as reference in the 'Startingpoint' field, use that
 			// Otherwise use the page's id-number from TSFE
 		$menuPid = intval($this->cObj->data['pages'] ? $this->cObj->data['pages'] : $GLOBALS['TSFE']->id);
 	
+			// Additional settings with TS
+		if (intval($conf['includeNotInMenu']) == 0) {
+			$addWhere = 'AND nav_hide=0';
+		}	
+		if ($conf['addWhere']) {
+			$addWhere .= ' ' . trim(htmlspecialchars($conf['addWhere']));
+		}
+			
 			// Now, get an array with all the subpages to this pid:
 			// (Function getMenu() is found in class.t3lib_page.php)
-		$menuItems_level1 = $GLOBALS['TSFE']->sys_page->getMenu($menuPid);
+		$menuItems_level1 = $GLOBALS['TSFE']->sys_page->getMenu($menuPid, $fields='*', $sortField='sorting', $addWhere, $checkShortcuts=1);
 	
 			// Prepare vars:
 		$items = array();
-	    $aChar = array();
-        
-        
-        foreach ($menuItems_level1 as $uid => $rec) {
-            $char = t3lib_div::strtoupper(substr($rec['title'],0,1));
-            if (ord($char)<65) {
-                $char = '0';
-            }
-            $aChar[$char] = 1;
-            $title = $rec['nav_title'] ? $rec['nav_title'] : $rec['title'];
-            $items[$char][$title] = $this->cObj->stdWrap($this->pi_linkToPage(
-                $title,  
-                $rec['uid'],
+		$aChar = array();
+		
+		
+		foreach ($menuItems_level1 as $uid => $rec) {
+			if (t3lib_div::compat_version('4.2')) {
+				$char = t3lib_div::strtoupper(substr($rec['title'],0,1));
+			} else {
+				$char = $this->strtoupper(substr($rec['title'],0,1));
+			}
+			if (ord($char)<65) {
+				$char = '0';
+			}
+			$aChar[$char] = 1;
+			$title = $rec['nav_title'] ? $rec['nav_title'] : $rec['title'];
+			$items[$char][$title] = $this->cObj->stdWrap($this->pi_linkToPage(
+				$title,  
+				$rec['uid'],
 				$rec['target']
-            ), $conf['pages.']);
-                
-        }
-        ksort($items);
-       
-        
-        // alphabetic chars
-        //all
-        $charlinks = $this->cObj->stdWrap($this->pi_linkTP($this->pi_getLL('all'), array(), 1), $conf['chars.']);
-        //0-9
-        $charlinks .= $this->cObj->stdWrap($aChar['0'] ? $this->pi_linkTP('0-9', array($this->prefixId.'[char]' => 0), 1) : '0-9', $conf['chars.']);
-        //chars
-        for ($i = 65; $i < 91; $i++) {
-            $charlinks .= $this->cObj->stdWrap($aChar[chr($i)] ? $this->pi_linkTP(chr($i), array($this->prefixId.'[char]' => chr($i)), 1) : chr($i), $conf['chars.']);
-        }
-        
+			), $conf['pages.']);
+				
+		}
+		ksort($items);
+	   
+		
+		// alphabetic chars
+		//all
+		$charlinks = $this->cObj->stdWrap($this->pi_linkTP($this->pi_getLL('all'), array(), 1), $conf['chars.']);
+		//0-9
+		$charlinks .= $this->cObj->stdWrap($aChar['0'] ? $this->pi_linkTP('0-9', array($this->prefixId.'[char]' => 0), 1) : '0-9', $conf['chars.']);
+		//chars
+		for ($i = 65; $i < 91; $i++) {
+			$charlinks .= $this->cObj->stdWrap($aChar[chr($i)] ? $this->pi_linkTP(chr($i), array($this->prefixId.'[char]' => chr($i)), 1) : chr($i), $conf['chars.']);
+		}
+		
 		
 		$totalMenu = $this->cObj->stdWrap($charlinks, $conf['charMenu.']);
-        
-        if ($this->piVars['char']) {
-            $totalMenu .= $this->cObj->stdWrap($this->piVars['char'], $conf['titleChar.']);   
-            $p = $items[$this->piVars['char']];
-            ksort($p, SORT_LOCALE_STRING);
-            $totalMenu .= $this->cObj->stdWrap(implode('', $p), $conf['pageMenu.']);   
-        } else {
-            foreach($items as $ch => $val) {
-                if ($val) {
-                    $totalMenu .= $this->cObj->stdWrap($ch, $conf['titleChar.']); 
-                    ksort($val, SORT_LOCALE_STRING);
-                    $totalMenu .= $this->cObj->stdWrap(implode('', $val), $conf['pageMenu.']);               
-                }   
-            }
-        }
+		
+		if ($this->piVars['char']) {
+			$totalMenu .= $this->cObj->stdWrap($this->piVars['char'], $conf['titleChar.']);   
+			$p = $items[$this->piVars['char']];
+			ksort($p, SORT_LOCALE_STRING);
+			$totalMenu .= $this->cObj->stdWrap(implode('', $p), $conf['pageMenu.']);   
+		} else {
+			foreach($items as $ch => $val) {
+				if ($val) {
+					$totalMenu .= $this->cObj->stdWrap($ch, $conf['titleChar.']); 
+					ksort($val, SORT_LOCALE_STRING);
+					$totalMenu .= $this->cObj->stdWrap(implode('', $val), $conf['pageMenu.']);               
+				}   
+			}
+		}
 	
 		return $this->pi_wrapInBaseClass($totalMenu);
 	}
 	
+	private function strtoupper($str) {
+		return strtr((string)$str, 'abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+	}
 
 }
 
