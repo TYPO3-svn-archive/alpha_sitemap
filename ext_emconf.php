7e622b51e84487fac140f377b80cce8e2f19d542
steffenk <steffenk@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-31 09:45:07 +0000

    ter release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/alpha_sitemap/trunk@15384 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "alpha_sitemap"
 #
-# Auto generated 27-10-2008 15:26
+# Auto generated 11-12-2008 13:11
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.2',
+	'version' => '0.0.3',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"ae90";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"8841";s:17:"ext_localconf.php";s:4:"01c1";s:14:"ext_tables.php";s:4:"cc91";s:24:"ext_typoscript_setup.txt";s:4:"905b";s:16:"locallang_db.xml";s:4:"fdd6";s:14:"doc/manual.sxw";s:4:"8185";s:19:"doc/wizard_form.dat";s:4:"7950";s:20:"doc/wizard_form.html";s:4:"b5e8";s:33:"pi1/class.tx_alphasitemap_pi1.php";s:4:"7721";s:17:"pi1/locallang.xml";s:4:"ea2c";}',
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"ae90";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"8841";s:17:"ext_localconf.php";s:4:"01c1";s:14:"ext_tables.php";s:4:"1006";s:24:"ext_typoscript_setup.txt";s:4:"ea7b";s:16:"locallang_db.xml";s:4:"fdd6";s:14:"doc/manual.sxw";s:4:"01dc";s:19:"doc/wizard_form.dat";s:4:"7950";s:20:"doc/wizard_form.html";s:4:"b5e8";s:33:"pi1/class.tx_alphasitemap_pi1.php";s:4:"0757";s:17:"pi1/locallang.xml";s:4:"ea2c";}',
 	'suggests' => array(
 	),
 );
