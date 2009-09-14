d57535f2d36c90b0dca6ad9ef03fe9f510b28cce
steffenk <steffenk@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-09-14 17:39:37 +0000

    fixed Bug #4617: wrong sorting of 0-9
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/alpha_sitemap/trunk@24470 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_alphasitemap_pi1.php
+++ pi1/class.tx_alphasitemap_pi1.php
@@ -43,8 +43,8 @@ class tx_alphasitemap_pi1 extends tslib_pibase {
 	var $extKey        = 'alpha_sitemap';	// The extension key.
 	var $pi_checkCHash = true;
 	var $addWhere;
-    var $menu;
-    	
+	var $menu;
+
 	/**
 	 * The main method of the PlugIn
 	 *
@@ -52,18 +52,18 @@ class tx_alphasitemap_pi1 extends tslib_pibase {
 	 * @param	array		$conf: The PlugIn configuration
 	 * @return	The content that is displayed on the website (Menu)
 	 */
-	function main($content, $conf)	{
+	public function main($content, $conf)	{
 		$this->pi_setPiVarDefaults();
 		$this->pi_loadLL();
 		$this->conf = $conf;
-		
+
 			// Get the PID from which to make the menu.
 			// If a page is set as reference in the 'Startingpoint' field, use that
 			// Otherwise use the page's id-number from TSFE
 		$menuPid = intval($this->cObj->data['pages']) ? $this->cObj->data['pages'] : $GLOBALS['TSFE']->id;
-	    $this->maxLevels = intval($this->cObj->data['recursive']) ? $this->cObj->data['recursive'] : 1;
-	    $this->menu = array();
-	    
+		$this->maxLevels = intval($this->cObj->data['recursive']) ? $this->cObj->data['recursive'] : 1;
+		$this->menu = array();
+
 			// Additional settings with TS
 		if (intval($this->conf['includeNotInMenu']) == 0) {
 			$this->addWhere = 'AND nav_hide=0';
@@ -88,14 +88,14 @@ class tx_alphasitemap_pi1 extends tslib_pibase {
 				$char = $this->strtoupper(substr($page['title'],0,1));
 			}
 			if (ord($char)<65) {
-				$char = '0';
+				$char = ' ';
 			}
 			$aChar[$char] = 1;
 			$title = $page['nav_title'] ? $page['nav_title'] : $page['title'];
 			if ($this->conf['titleField']) {
 				$title = $page[$this->conf['titleField']];
 			}
-			// complete record to data to enable access to other fields in TS
+				// complete record to data to enable access to other fields in TS
 			$this->cObj->data = $page;
 			$items[$char][$title] = $this->cObj->stdWrap($this->pi_linkToPage(
 				$title,  
@@ -105,59 +105,59 @@ class tx_alphasitemap_pi1 extends tslib_pibase {
 			
 		}
 		ksort($items);
-	   
-		
+
+
 		// alphabetic chars
 		//all
 		$charlinks = $this->cObj->stdWrap($this->pi_linkTP($this->pi_getLL('all'), array(), 1), $this->conf['chars.']);
 		//0-9
-		$charlinks .= $this->cObj->stdWrap($aChar['0'] ? $this->pi_linkTP('0-9', array($this->prefixId.'[char]' => 0), 1) : '0-9', $this->conf['chars.']);
+		$charlinks .= $this->cObj->stdWrap($aChar[' '] ? $this->pi_linkTP('0-9', array($this->prefixId.'[char]' => 0), 1) : '0-9', $this->conf['chars.']);
 		//chars
 		for ($i = 65; $i < 91; $i++) {
 			if ($this->piVars['char'] == chr($i)) {
 				if ($this->conf['activeChar.']['doNotLink']) { 
-					$charlinks .= $this->cObj->stdWrap(chr($i), $this->conf['activeChar.']);				 	
+					$charlinks .= $this->cObj->stdWrap(chr($i), $this->conf['activeChar.']);
 				} else {
 					$ATagParams = $GLOBALS['TSFE']->ATagParams;
-					$GLOBALS['TSFE']->ATagParams = $this->conf['activeChar.']['ATagParams'];					
+					$GLOBALS['TSFE']->ATagParams = $this->conf['activeChar.']['ATagParams'];
 					$charlinks .= $this->cObj->stdWrap($this->pi_linkTP(chr($i), array($this->prefixId.'[char]' => chr($i)), 1), $this->conf['activeChar.']);		
 					$GLOBALS['TSFE']->ATagParams = $ATagParams;
 				}
 			} else {
-			    $charlinks .= $this->cObj->stdWrap($aChar[chr($i)] ? $this->pi_linkTP(chr($i), array($this->prefixId.'[char]' => chr($i)), 1) : chr($i), $this->conf['chars.']);
+				$charlinks .= $this->cObj->stdWrap($aChar[chr($i)] ? $this->pi_linkTP(chr($i), array($this->prefixId.'[char]' => chr($i)), 1) : chr($i), $this->conf['chars.']);
 			} 
 		}
-		
-		
+
+
 		$totalMenu = $this->cObj->stdWrap($charlinks, $this->conf['charMenu.']);
-		
+
 		if ($this->piVars['char']) {
-			$totalMenu .= $this->cObj->stdWrap($this->piVars['char'], $this->conf['titleChar.']);   
+			$totalMenu .= $this->cObj->stdWrap($this->piVars['char'], $this->conf['titleChar.']);
 			$p = $items[$this->piVars['char']];
 			ksort($p, SORT_LOCALE_STRING);
-			$totalMenu .= $this->cObj->stdWrap(implode('', $p), $this->conf['pageMenu.']);   
+			$totalMenu .= $this->cObj->stdWrap(implode('', $p), $this->conf['pageMenu.']);
 		} else {
 			foreach($items as $ch => $val) {
 				if ($val) {
-					$totalMenu .= $this->cObj->stdWrap($ch, $this->conf['titleChar.']); 
+					$totalMenu .= $this->cObj->stdWrap($ch == ' ' ? '0-9' : $ch, $this->conf['titleChar.']);
 					ksort($val, SORT_LOCALE_STRING);
-					$totalMenu .= $this->cObj->stdWrap(implode('', $val), $this->conf['pageMenu.']);               
-				}   
+					$totalMenu .= $this->cObj->stdWrap(implode('', $val), $this->conf['pageMenu.']);
+				}
 			}
 		}
-	
+
 		return $this->pi_wrapInBaseClass($totalMenu);
 	}
-	
+
 	/**
-    *
-    * @param string $str
-    * @return string
-    */
-    private function strtoupper($str) {
+	*
+	* @param string $str
+	* @return string
+	*/
+	private function strtoupper($str) {
 		return strtr((string)$str, 'abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 	}
-	
+
 	/**
 	* recursive functions to catch all pages for menu
 	* 
@@ -165,16 +165,16 @@ class tx_alphasitemap_pi1 extends tslib_pibase {
 	* @param int $level	don't use it with the call of the function, it is used by the function for recursive calls
 	* @return
 	*/
-	function recursiveMenu($pid, $level = 1) {
-		// (Function getMenu() is found in class.t3lib_page.php)        
+	protected function recursiveMenu($pid, $level = 1) {
+			// (Function getMenu() is found in class.t3lib_page.php)
 		$menu = $GLOBALS['TSFE']->sys_page->getMenu($pid, $fields='*', $sortField='sorting', $this->addWhere, $checkShortcuts=1);
 		$this->menu = array_merge($menu, $this->menu);
 		foreach($menu as $uid => $page) {
-		    if ($level < $this->maxLevels) {
+			if ($level < $this->maxLevels) {
 				$this->recursiveMenu($uid, $level + 1);
-		    }
+			}
 		}
-    }
+	}
 
 }
 
